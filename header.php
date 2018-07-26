3fd87b436c04df66a9ede69ed6d68d9b0fab238b
Jorge <jorge.silva8686@gmail.com>
2018-07-26 16:52:34 -0400

    Initial commit

---
+++ header.php
@@ -0,0 +1,4 @@
+<script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.js" type="text/javascript"></script>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>Inversiones Luis Rojas SPA</title>
+	<script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+	
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+	<div class="container-fluid">
+	    <div class="row">
+		<div class="col-md-12">
+		    <div class="page-header" style='text-align: center;'>
+			<h1>
+			    Sitio en construcción
+			    <div>
+				<small>Inversiones Luis Rojas SPA</small>
+			    </div>
+			</h1>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </body>
+</html>
